7fab7b8202881348bffb2c9205da23311cb8c449
cppaddiction <cppaddiction@yandex.ru>
2024-07-24 14:31:53 +0800

    final_task

---
+++ sprint1/problems/final_task/solution/src/json_loader.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <filesystem>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <boost/json.hpp>
+#include "model.h"
+
+namespace json_loader {
+
+model::Game LoadGame(const std::filesystem::path& json_path);
+
+}  // namespace json_loader
