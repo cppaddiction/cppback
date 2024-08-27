27cab4ee4bf02f75b7294b223a04ee884aa2d39b
cppaddiction <cppaddiction@yandex.ru>
2024-08-27 19:33:41 +0800

    command_line

---
+++ sprint2/problems/command_line/solution/src/app.h
@@ -0,0 +1,139 @@
+#pragma once
+#include "model.h"
+#include <random>
+#include <iostream>
+#include <unordered_map>
+
+namespace app {
+    using Token = util::Tagged<std::string, detail::TokenTag>;
+
+    class PlayerToken {
+    public:
+        PlayerToken() {  
+            strm_ << std::hex << generator1_() << generator2_();
+            int x = strm_.str().size();
+            if (x < 32)
+            {
+                for (int i = 0; i < 32 - x; i++)
+                {
+                    strm_ << '0';
+                }
+            }
+        }
+        Token GetToken() const { return Token{ strm_.str() }; }
+    private:
+        std::random_device random_device_;
+        std::mt19937_64 generator1_{ [this] {
+            std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+            return dist(random_device_);
+        }() };
+        std::mt19937_64 generator2_{ [this] {
+            std::uniform_int_distribution<std::mt19937_64::result_type> dist;
+            return dist(random_device_);
+        }() };
+        // Чтобы сгенерировать токен, получите из generator1_ и generator2_
+        // два 64-разрядных числа и, переведя их в hex-строки, склейте в одну.
+        // Вы можете поэкспериментировать с алгоритмом генерирования токенов,
+        // чтобы сделать их подбор ещё более затруднительным
+        std::ostringstream strm_;
+    };
+
+	class Player {
+    public:
+        Player() : token_(std::make_unique<PlayerToken>()->GetToken()) {}
+        Player(model::Dog dog, std::shared_ptr<model::GameSession> session, Token token) : dog_(dog), session_(session), token_(token) {}
+        const model::Dog& GetDog() const { return dog_; }
+        void SetDir(std::string dir) { dog_.SetDir(dir); (session_->FindDog(dog_.GetName(), dog_.GetId())).SetDir(dir); }
+        void SetSpeed(int vX, int vY, double dds) 
+        { 
+            auto map = session_->GetMap(); 
+            double vxx = vX * (map.GetSpecificMapDogSpeed() ? map.GetSpecificMapDogSpeed() : dds);
+            double vyy = vY * (map.GetSpecificMapDogSpeed() ? map.GetSpecificMapDogSpeed() : dds);
+            dog_.SetSpeed(vxx, vyy);
+            (session_->FindDog(dog_.GetName(), dog_.GetId())).SetSpeed(vxx, vyy);
+        }
+        void SetSpeed(std::string dir, double game_default_speed) {
+            if (dir == "U")
+            {
+                SetSpeed(0, -1, game_default_speed);
+            }
+            else if (dir == "D")
+            {
+                SetSpeed(0, 1, game_default_speed);
+            }
+            else if (dir == "L")
+            {
+                SetSpeed(-1, 0, game_default_speed);
+            }
+            else if (dir == "R")
+            {
+                SetSpeed(1, 0, game_default_speed);
+            }
+            else
+            {
+                SetSpeed(0, 0, game_default_speed);
+            }
+        }
+        void SyncronizeSession() {
+            dog_ = (session_->FindDog(dog_.GetName(), dog_.GetId()));
+        }
+        const model::GameSession& GetSession() const { return *session_; }
+        Token GetAuthToken() const { return token_; }
+	private:
+        model::Dog dog_;
+        std::shared_ptr<model::GameSession> session_;
+		Token token_;
+	};
+
+	class Players {
+        struct PlayerInfo {
+            bool operator==(const PlayerInfo& other) const { return dog_id == other.dog_id && map_id == other.map_id; }
+            std::uint64_t dog_id;
+            std::string map_id;
+        };
+        struct PlayerInfoHasher {
+            size_t operator()(const PlayerInfo& pi) const {
+                return std::hash<std::uint64_t>{}(pi.dog_id) + std::hash<std::string>{}(pi.map_id);
+            }
+        };
+    public:
+        Player& Addplayer(model::Dog dog, std::shared_ptr<model::GameSession> session)
+        {
+            auto token = std::make_unique<PlayerToken>()->GetToken();
+            players_by_info_.try_emplace(PlayerInfo{ dog.GetId(), *((session->GetMap()).GetId()) }, dog, session, token);
+            players_by_token_.try_emplace(token, dog, session, token);
+            return players_by_token_[token];
+        }
+        Player& FindByDogIdAndMapId(std::uint64_t dog_id, std::string map_id)
+        {
+            if (auto search = players_by_info_.find(PlayerInfo{ dog_id, map_id }); search != players_by_info_.end())
+            {
+                return players_by_token_[(search->second).GetAuthToken()];
+            }
+            else
+            {
+                throw std::logic_error("No such player");
+            }
+        }
+        Player& FindByToken(Token token)
+        {
+            if (auto search = players_by_token_.find(token); search != players_by_token_.end())
+            {
+                return players_by_token_[search->first];
+            }
+            else
+            {
+                throw std::logic_error("No such player");
+            }
+        }
+        void SyncronizeSession() {
+            for (auto it = players_by_token_.begin(); it != players_by_token_.end(); it++)
+            {
+                (it->second).SyncronizeSession();
+            }
+        }
+    private:
+        std::unordered_map<PlayerInfo, Player, PlayerInfoHasher> players_by_info_;
+        std::unordered_map<Token, Player, util::TaggedHasher<Token>> players_by_token_;
+	};
+}
---
+++ sprint2/problems/command_line/solution/src/json_loader.cpp
@@ -0,0 +1,88 @@
+#include "json_loader.h"
+
+namespace json = boost::json;
+using namespace std::literals;
+
+namespace json_loader {
+
+void LoadMaps(const json::array& maps, model::Game& game)
+{
+    for (const auto& m : maps)
+    {
+        model::Map map(model::Map::Id(static_cast<std::string>(m.as_object().at(ID).as_string())), static_cast<std::string>(m.as_object().at(NAME).as_string()));
+        ::boost::json::value const* const p_value{ m.as_object().if_contains(MAP_DOG_SPEED) };
+        if (p_value)
+        {
+            map.AddSpecificMapDogSpeed(m.as_object().at(MAP_DOG_SPEED).as_double());
+        }
+        LoadRoads(m.as_object().at(ROADS).as_array(), map);
+        LoadBuildings(m.as_object().at(BUILDINGS).as_array(), map);
+        LoadOffices(m.as_object().at(OFFICES).as_array(), map);
+        game.AddMap(map);
+    }
+}
+void LoadRoads(const json::array& roads, model::Map& map)
+{
+    for (const auto& r : roads)
+    {
+        ::boost::json::value const* const p_value{ r.as_object().if_contains(X1) };
+        if (p_value)
+        {
+            map.AddRoad(model::Road{ model::Road::HORIZONTAL, model::Point{static_cast<int>(r.as_object().at(X0).as_int64()), static_cast<int>(r.as_object().at(Y0).as_int64())},  static_cast<int>(r.as_object().at(X1).as_int64()) });
+        }
+        else
+        {
+            map.AddRoad(model::Road{ model::Road::VERTICAL, model::Point{static_cast<int>(r.as_object().at(X0).as_int64()), static_cast<int>(r.as_object().at(Y0).as_int64())},  static_cast<int>(r.as_object().at(Y1).as_int64()) });
+        }
+    }
+    map.CreateRoadGrid();
+}
+void LoadBuildings(const json::array& buildings, model::Map& map)
+{
+    for (const auto& b : buildings)
+    {
+        map.AddBuilding(model::Building{ model::Rectangle{model::Point{static_cast<int>(b.as_object().at(X).as_int64()), static_cast<int>(b.as_object().at(Y).as_int64())}, model::Size{static_cast<int>(b.as_object().at(W).as_int64()), static_cast<int>(b.as_object().at(H).as_int64())}} });
+    }
+}
+void LoadOffices(const json::array& offices, model::Map& map)
+{
+    for (const auto& o : offices)
+    {
+        map.AddOffice(model::Office{ model::Office::Id(static_cast<std::string>(o.as_object().at(ID).as_string())), model::Point{static_cast<int>(o.as_object().at(X).as_int64()), static_cast<int>(o.as_object().at(Y).as_int64())}, model::Offset{static_cast<int>(o.as_object().at(OffsetX).as_int64()), static_cast<int>(o.as_object().at(OffsetY).as_int64())} });
+    }
+}
+model::Game LoadGame(const std::filesystem::path& json_path) {
+    // Загрузить содержимое файла json_path, например, в виде строки
+    // Распарсить строку как JSON, используя boost::json::parse
+    // Загрузить модель игры из файла
+
+    if (std::ifstream in{ json_path })
+    {
+        std::string file, line;
+
+        while (getline(in, line))
+        {
+            file += line;
+        }
+
+        auto value = json::parse(file);
+        auto maps=value.as_object().at(MAPS).as_array();
+
+        model::Game game;
+
+        ::boost::json::value const* const p_value{ value.as_object().if_contains(DEFAULT_DOG_SPEED) };
+        if (p_value)
+        {
+            game.AddDefaultDogSpeed(value.as_object().at(DEFAULT_DOG_SPEED).as_double());
+        }
+
+        LoadMaps(maps, game);
+        return game;
+    }
+    else
+    {
+        throw std::logic_error(INVALID_FILE_PATH);
+    }
+}
+
+}  // namespace json_loader
