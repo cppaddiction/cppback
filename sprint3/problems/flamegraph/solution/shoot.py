38af69460bfa3701179acf02fc838a9f8c9527f6
cppaddiction <cppaddiction@yandex.ru>
2024-08-30 23:13:38 +0800

    flamegraph

---
+++ sprint3/problems/flamegraph/solution/shoot.py
@@ -0,0 +1,62 @@
+import argparse
+import subprocess
+import time
+import random
+import shlex
+import os
+
+RANDOM_LIMIT = 1000
+SEED = 123456789
+random.seed(SEED)
+
+AMMUNITION = [
+    'localhost:8080/api/v1/maps/map1',
+    'localhost:8080/api/v1/maps'
+]
+
+SHOOT_COUNT = 100
+COOLDOWN = 0.1
+
+
+def start_server():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('server', type=str)
+    return parser.parse_args().server
+
+
+def run(command, output=None):
+    process = subprocess.Popen(shlex.split(command), stdout=output, stderr=subprocess.DEVNULL)
+    return process
+
+
+def stop(process, wait=False):
+    if process.poll() is None and wait:
+        process.wait()
+    process.terminate()
+
+
+def shoot(ammo):
+    hit = run('curl ' + ammo, output=subprocess.DEVNULL)
+    time.sleep(COOLDOWN)
+    stop(hit, wait=True)
+
+
+def make_shots():
+    for _ in range(SHOOT_COUNT):
+        ammo_number = random.randrange(RANDOM_LIMIT) % len(AMMUNITION)
+        shoot(AMMUNITION[ammo_number])
+    print('Shooting complete')
+
+
+server = run(start_server())
+record_command = 'sudo perf record -p ' + str(server.pid) + ' -g -o perf.data'
+record = run(record_command)
+make_shots()
+stop(server)
+stop(record)
+#flamegraph_command = 'sudo perf script -i perf.data | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl > graph.svg'
+#flamegraph = run(flamegraph_command)
+#stop(flamegraph)
+os.system('sudo perf script -i perf.data | ./FlameGraph/stackcollapse-perf.pl | ./FlameGraph/flamegraph.pl > graph.svg')
+time.sleep(1)
+print('Job done')
